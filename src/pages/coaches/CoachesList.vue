b7fc0d18a97b6d5ca5d822d16bd8edf1170079a8
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2023-09-06 22:51:43 +0100

    communication between CoachesList and CoachItem components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -9,22 +9,45 @@
     </div>
     <h2>LIST OF COACHES</h2>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
     },
-    hasCoaches(){
-        return this.$store.getters['coaches/hasCoaches'];
-    }
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
   },
 };
 </script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
