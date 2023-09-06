952619d6fdc6f9a8cb1cb0cab8eff89344a2efc1
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2023-09-06 19:05:35 +0100

    Adding Route Components

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,8 @@
+<template>
+    <h2>Register</h2>
+</template>
+<script>
+export default{
+    
+}
+</script>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,8 @@
+<template>
+    <h2>Coaches</h2>
+</template>
+<script>
+export default{
+
+}
+</script>
