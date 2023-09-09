2af8bea9ffb6a0ca68fcd0cc70bf1fd330122317
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2023-09-09 23:30:30 +0100

    added request form and validation + connection to vuex + requests outputting

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,8 +1,43 @@
 <template>
-    <h2>Requests Received</h2>
+  <section>
+    <base-card>
+      <header><h2>Requests Received</h2></header>
+      <ul v-if="hasRequest">
+          <request-item v-for="request in receivedRequests" :key="request.id" :email="request.userEmail"  :message="request.message"></request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
 <script>
-export default{
-    
-}
+import RequestItem from '../../components/requests/RequestItem.vue';
+export default {
+  components: {
+    RequestItem,
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/getRequests'];
+    },
+    hasRequest() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
 </script>
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,21 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
 const store = createStore({
   modules: {
     coaches: coachesModule,
+    requests: requestsModule,
   },
   state() {
     return {
       userId: 'c3',
     };
   },
-  getters:{
-    userid(state){
-        return state.userId
-    }
-  }
+  getters: {
+    userid(state) {
+      return state.userId;
+    },
+  },
 });
 
 export default store;
