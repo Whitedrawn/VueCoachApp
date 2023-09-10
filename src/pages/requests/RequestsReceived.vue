ec2b48e853c88f5a78b01021221f7c81fe65291a
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2023-09-10 22:39:14 +0100

    Http requests for Requests data

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,9 +1,18 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header><h2>Requests Received</h2></header>
-      <ul v-if="hasRequest">
-          <request-item v-for="request in receivedRequests" :key="request.id" :email="request.userEmail"  :message="request.message"></request-item>
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequest && !isLoading ">
+        <request-item
+          v-for="request in receivedRequests"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.message"
+        ></request-item>
       </ul>
       <h3 v-else>You haven't received any requests yet!</h3>
     </base-card>
@@ -12,6 +21,12 @@
 <script>
 import RequestItem from '../../components/requests/RequestItem.vue';
 export default {
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   components: {
     RequestItem,
   },
@@ -22,6 +37,24 @@ export default {
     hasRequest() {
       return this.$store.getters['requests/hasRequests'];
     },
+  },
+  created(){
+    this.loadRequests()
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed!';
+      }
+
+      this.isLoading = false;
+    },
+    handleError(){
+      this.error=null
+    }
   },
 };
 </script>
